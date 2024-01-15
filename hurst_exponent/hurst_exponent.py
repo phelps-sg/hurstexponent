3ac026b48bb99bbe4fd98b46b4f50ef424b578dc
Steve Phelps <phelps.sg@gmail.com>
2024-01-15 19:40:09 +0000

    raise exception rather than returning nan in the case of no valid lags

--- hurst_exponent/hurst_exponent.py
+++ hurst_exponent/hurst_exponent.py
@@ -81,7 +81,7 @@ def standard_hurst(
     )
 
     if not valid_lags or not y_values:
-        return np.nan, np.nan, [[], []]
+        raise ValueError("No valid lags")
 
     # Fit and return Hurst
     xy_df = pd.DataFrame({"x_values": valid_lags, "y_values": y_values})
@@ -138,7 +138,7 @@ def generalized_hurst(
     valid_lags = [lag for lag in lag_sizes if np.isfinite(structure_function(series, moment, lag))]
 
     if not valid_lags or not S_q_tau_values:
-        return np.nan, np.nan, [[], []]
+        raise ValueError("No valid lags")
 
     # Fit and return Hurst
     xy_df = pd.DataFrame({"x_values": valid_lags, "y_values": S_q_tau_values})
